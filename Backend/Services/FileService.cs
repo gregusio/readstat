8488fb69e56fa2082cce9469dac0d7bc9a50d8e5
gregusio <grzegorzdynaq@gmail.com>
2025-01-02 23:28:25 +0100

    Refactor FileService to use repositories for processing CSV files and managing user book records

--- Backend/Services/FileService.cs
+++ Backend/Services/FileService.cs
@@ -1,18 +1,86 @@
 using System.Globalization;
+using Backend.Models;
+using Backend.Repositories;
 using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace Backend.Services;
 
-public class FileService
+public class FileService(BooksRepository booksRepository, UserBookRecordsRepository userBookRecordsRepository)
 {
-    public async Task<int> ProcessCsvFile(IFormFile file)
-    {
-        using var reader = new StreamReader(file.OpenReadStream());
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        var records = csv.GetRecords<dynamic>().ToList();
-        // TODO: Process records
-        await Task.CompletedTask;
+    private readonly BooksRepository _booksRepository = booksRepository;
+    private readonly UserBookRecordsRepository _userBookRecordsRepository = userBookRecordsRepository;
 
-        return records.Count;
+    public async Task<int> ProcessCsvFile(IFormFile file, int userId)
+    {
+        var conf = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+        };
+        using var stream = new StreamReader(file.OpenReadStream());
+        using var csvReader = new CsvReader(stream, conf);
+
+        csvReader.Read();
+        csvReader.ReadHeader();
+
+        var bookRecords = new List<UserBookRecord>();
+
+        while (csvReader.Read())
+        {
+            var book = await FindOrCreateBook(csvReader);
+
+            var userBookRecord = new UserBookRecord
+            {
+                UserId = userId,
+                BookId = book.Id,
+                Book = book,
+                MyRating = csvReader.GetField<int?>("My Rating"),
+                ExclusiveShelf = csvReader.GetField("Exclusive Shelf")!,
+                DateRead = DateTime.TryParse(csvReader.GetField("Date Read"), out var dateRead) ? dateRead : null,
+                DateAdded = DateTime.TryParse(csvReader.GetField("Date Added"), out var dateAdded) ? dateAdded : null,
+                MyReview = csvReader.GetField("My Review"),
+                ReadCount = csvReader.GetField<int>("Read Count"),
+            };
+
+            bookRecords.Add(userBookRecord);
+        }
+
+        await _userBookRecordsRepository.AddRangeAsync(bookRecords);
+
+        return bookRecords.Count;
+    }
+
+    private async Task<Book> FindOrCreateBook(CsvReader csvReader)
+    {
+        var isbn = NormalizeIsbn(csvReader.GetField("ISBN")!);
+
+
+        var book = await _booksRepository.GetByIsbnAsync(isbn);
+
+        if (book == null)
+        {
+            book = new Book
+            {
+                Title = csvReader.GetField("Title"),
+                Author = csvReader.GetField("Author"),
+                AdditionalAuthors = csvReader.GetField("Additional Authors"),
+                ISBN = isbn,
+                ISBN13 = NormalizeIsbn(csvReader.GetField("ISBN13")!),
+                AverageRating = csvReader.GetField<double>("Average Rating"),
+                Publisher = csvReader.GetField("Publisher"),
+                NumberOfPages = csvReader.GetField<int?>("Number of Pages"),
+                YearPublished = csvReader.GetField<int?>("Year Published"),
+                OriginalPublicationYear = csvReader.GetField<int?>("Original Publication Year"),
+            };
+
+            // await _booksRepository.AddAsync(book);
+        }
+
+        return book;
+    }
+
+    private string NormalizeIsbn(string isbn)
+    {
+        return isbn.Replace("=", "").Replace("\"", "");
     }
 }
