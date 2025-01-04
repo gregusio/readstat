aa56303feffc181cfa080c2ca23a21fb6baa4f0a
gregusio <grzegorzdynaq@gmail.com>
2025-01-04 01:35:25 +0100

    Add BookBasicInfoDTO class for basic book information representation

---
+++ Backend/DTO/BookBasicInfoDTO.cs
@@ -0,0 +1,9 @@
+namespace Backend.DTO;
+
+public class BookBasicInfoDTO
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? ExclusiveShelf { get; set; }
+}
