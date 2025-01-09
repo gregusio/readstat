9094919ecf0b43caeb127069e77652f5e30edd57
gregusio <grzegorzdynaq@gmail.com>
2025-01-09 23:07:45 +0100

    Refactor ProtectedRoute to use authentication context and add new routes for Home and Books pages

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,12 +4,15 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ProtectedRoute from "./ProtectedRoute";
+import Books from "../pages/Books";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route element={<ProtectedRoute />}>
         <Route path="*" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/books" element={<Books />} />
       </Route>
 
       <Route path="/login" element={<Login />} />
