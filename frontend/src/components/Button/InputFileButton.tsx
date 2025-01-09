d0e985fa5f4267ba3dea0b99ec62519173684848
gregusio <grzegorzdynaq@gmail.com>
2025-01-09 23:06:19 +0100

    Add file upload component and integrate it into Books page

---
+++ frontend/src/components/Button/InputFileButton.tsx
@@ -0,0 +1,46 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import fileService from '../../services/fileService';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+export default function InputFileUpload() {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files[0]) {
+      var response = fileService.uploadCsv(files[0]);
+      console.log(response);
+    }
+
+  };
+    
+
+
+  return (
+    <Button
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+      startIcon={<CloudUploadIcon />}
+    >
+      Upload files
+      <VisuallyHiddenInput
+        type="file"
+        onChange={(event) => handleFileChange(event)}
+        multiple
+      />
+    </Button>
+  );
+}
---
+++ frontend/src/pages/Books.tsx
@@ -0,0 +1,15 @@
+import InputFileUpload from "../components/Button/InputFileButton";
+
+const Books: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <h1>Books</h1>
+      </div>
+
+      <InputFileUpload />
+    </div>
+  );
+};
+
+export default Books;
