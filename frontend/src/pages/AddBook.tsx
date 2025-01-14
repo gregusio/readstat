e6d893df715f0b7331d3da4ea67bd090ea0aa4f2
gregusio <grzegorzdynaq@gmail.com>
2025-01-14 20:08:02 +0100

    Add AddBook page to integrate AddBookForm component

---
+++ frontend/src/pages/AddBook.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddBookForm from "../components/Form/AddBookForm";
+
+const AddBook: React.FC = () => {
+
+    return (
+        <AddBookForm />
+    );
+
+};
+
+export default AddBook;
