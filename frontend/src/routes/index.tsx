653e57c86f36dd482e903c05092854724b2aef73
gregusio <grzegorzdynaq@gmail.com>
2025-01-14 15:00:48 +0100

    Add Statistics route to enable navigation to the Statistics page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ProtectedRoute from "./ProtectedRoute";
 import Books from "../pages/Books";
 import BookDetail from "../pages/BookDetail";
+import Statistics from "../pages/Statistics";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const AppRoutes: React.FC = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/books" element={<Books />} />
         <Route path="/books/:id" element={<BookDetail />} />
+        <Route path="/statistics" element={<Statistics />} />
       </Route>
       
       <Route path="/login" element={<Login />} />
