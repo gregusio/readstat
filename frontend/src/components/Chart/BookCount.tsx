e562e7f3afa3487c9084962f08e9f750fe56da6f
gregusio <grzegorzdynaq@gmail.com>
2025-01-14 17:23:56 +0100

    Add BookCount component to display read and unread book statistics

---
+++ frontend/src/components/Chart/BookCount.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import statisticService from "../../services/statisticService";
+import { Skeleton, Typography } from "@mui/material";
+import { PieChart } from "@mui/x-charts";
+
+const BookCount: React.FC = () => {
+  const [chartData, setChartData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    statisticService.getSummary().then((response) => {
+      const { totalReadBooks, totalUnreadBooks } = response;
+      setChartData([
+        { label: "Read", value: totalReadBooks },
+        { label: "Unread", value: totalUnreadBooks, color: "gray" },
+      ]);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <Skeleton variant="rectangular" width={500} height={300} />;
+  }
+
+  return (
+    <>
+    <Typography variant="h6">Book Count</Typography>
+    <PieChart
+      series={[
+        {
+          data: chartData,
+          highlightScope: { fade: 'global', highlight: 'item' },
+          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+        },
+      ]}
+      height={200}
+    />
+    </>
+  );
+};
+
+export default BookCount;
