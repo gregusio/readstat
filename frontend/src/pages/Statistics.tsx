43a99ffbdaed63d04decd0c0c2aabf5ba7b91527
gregusio <grzegorzdynaq@gmail.com>
2025-01-15 15:59:10 +0100

    Refactor Statistics component for improved responsive layout

--- frontend/src/pages/Statistics.tsx
+++ frontend/src/pages/Statistics.tsx
@@ -14,30 +14,30 @@ const Statistics: React.FC = () => {
   return (
     <Box sx={{ flexGrow: 1, p: 2 }}>
       <Grid container spacing={10}>
-        <Grid size={6}>
+        <Grid size={{xs:12, sm: 6}}>
           <div className="center">
             <SummaryStatsCard />
           </div>
         </Grid>
-        <Grid size={6}>
+        <Grid size={{xs:12, sm: 6}}>
           <BookCount />
         </Grid>
-        <Grid size={6}>
+        <Grid size={{xs:12, sm: 6}}>
           <MonthlyReadBookCount />
         </Grid>
-        <Grid size={6}>
-          <MonthlyReadPageCount />
-        </Grid>
-        <Grid size={6}>
-          <MonthlyAddedBookCount />
-        </Grid>
-        <Grid size={6}>
+        <Grid size={{xs:12, sm: 6}}>
           <YearlyReadBookCount />
         </Grid>
-        <Grid size={6}>
+        <Grid size={{xs:12, sm: 6}}>
+          <MonthlyReadPageCount />
+        </Grid>
+        <Grid size={{xs:12, sm: 6}}>
           <YearlyReadPageCount />
         </Grid>
-        <Grid size={6}>
+        <Grid size={{xs:12, sm: 6}}>
+          <MonthlyAddedBookCount />
+        </Grid>
+        <Grid size={{xs:12, sm: 6}}>
           <YearlyAddedBookCount />
         </Grid>
       </Grid>
