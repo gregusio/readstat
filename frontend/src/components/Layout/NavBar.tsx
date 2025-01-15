d0a629a5ddefd2a8d52c3bc194d0d440c1eb3f6d
gregusio <grzegorzdynaq@gmail.com>
2025-01-15 15:59:04 +0100

    Refactor NavBar component for improved structure and responsiveness

--- frontend/src/components/Layout/NavBar.tsx
+++ frontend/src/components/Layout/NavBar.tsx
@@ -1,34 +1,45 @@
-import React, { useContext } from "react";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-  Tooltip,
-  IconButton,
-  Avatar,
-  Menu,
-  MenuItem,
-} from "@mui/material";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import InputFileUpload from "../Button/InputFileButton";
 import SearchBar from "../Bar/SearchBar";
 import { SearchContext } from "../../context/SearchContext";
+import { useContext } from "react";
 
 const pages = ["Home", "Books", "Statistics"];
+const settings = ["Profile", "Logout"];
 
-const NavBar: React.FC = () => {
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
   );
-  const { isAuthenticated, logout } = useAuth();
-
+  const { logout, isAuthenticated } = useAuth();
+  
   const navigate = useNavigate();
   const { clearSearchQuery } = useContext(SearchContext);
-
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
   };
 
   const handleCloseUserMenu = () => {
@@ -38,92 +49,171 @@ const NavBar: React.FC = () => {
   const handleGoToPage = (page: string) => {
     clearSearchQuery();
     navigate(`/${page.toLowerCase()}`);
+    handleCloseNavMenu();
     handleCloseUserMenu();
   };
 
-  const handleLogout = () => {
-    logout();
-    handleCloseUserMenu();
-    navigate("/login");
-  };
+  const handleSettings = (setting: string) => {
+    if (setting === "Logout") {
+      logout();
+      navigate("/login");
+    } else if (setting === "Profile") {
+      navigate("/profile");
+    }
 
-  const settings = [
-    { label: "Profile", action: () => handleGoToPage("Profile") },
-    { label: "Logout", action: handleLogout },
-  ];
+    clearSearchQuery();
+    handleCloseUserMenu();
+  };
 
   return (
     <AppBar position="fixed">
-      <Toolbar>
-        <img
-          src="/book_icon.png"
-          alt="book icon"
-          style={{
-            width: "30px",
-            height: "30px",
-            marginRight: "10px",
-            marginBottom: "5px",
-          }}
-        />
-        <Typography variant="h6" sx={{ marginRight: "20px" }}>
-          Readstat
-        </Typography>
-        {isAuthenticated ? (
-          <>
-            <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "left" }}>
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleGoToPage.bind(null, page)}>
-                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Box>
+      <Container maxWidth={false}>
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Readstat
+          </Typography>
 
-            <SearchBar />
-
-            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-              <InputFileUpload />
-              <Tooltip title="Click it!" placement="bottom">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/book_icon.png" />
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            {isAuthenticated && (
+              <>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
                 </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting.label} onClick={setting.action}>
-                    <Typography sx={{ textAlign: "center" }}>
-                      {setting.label}
-                    </Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          </>
-        ) : 
-        (
-          <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "right" }}>
-            <MenuItem onClick={() => navigate("/login")}>
-              <Typography sx={{ textAlign: "center" }}>Login</Typography>
-            </MenuItem>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{ display: { xs: "block", md: "none" } }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={() => handleGoToPage(page)}>
+                      <Typography sx={{ textAlign: "center" }}>
+                        {page}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            )}
           </Box>
-        )}
-      </Toolbar>
+          <img
+            src="/book_icon.png"
+            alt="logo"
+            style={{ width: "50px", height: "50px" }}
+          />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Readstat
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {isAuthenticated &&
+              pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={() => handleGoToPage(page)}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+          </Box>
+          {isAuthenticated ? (
+            <>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  display: { xs: "flex", md: "flex" },
+                  justifyContent: "center",
+                }}
+              >
+                <SearchBar />
+              </Box>
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Remy Sharp" src="/book_icon.png" />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem
+                      key={setting}
+                      onClick={() => handleSettings(setting)}
+                    >
+                      <Typography sx={{ textAlign: "center" }}>
+                        {setting}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </>
+          ) : (
+            <Button onClick={() => navigate("/login")}>Login</Button>
+          )}
+        </Toolbar>
+      </Container>
     </AppBar>
   );
-};
-
+}
 export default NavBar;
