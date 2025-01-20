3b5aec04409b459a6a063677823b738da062f584
gregusio <grzegorzdynaq@gmail.com>
2025-01-21 00:11:23 +0100

    Add help modal for file upload instructions in Books component

--- frontend/src/pages/Books.tsx
+++ frontend/src/pages/Books.tsx
@@ -2,10 +2,32 @@ import React, { useContext, useEffect, useState } from "react";
 import bookService from "../services/bookService";
 import BookCard from "../components/Card/BookCard";
 import { SearchContext } from "../context/SearchContext";
-import { Box, Button, Pagination, Skeleton } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Modal,
+  Pagination,
+  Skeleton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import Grid from "@mui/material/Grid2";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import InputFileUpload from "../components/Button/InputFileButton";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 interface Book {
   id: number;
@@ -18,6 +40,9 @@ const Books: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const booksPerPage = 24;
   const handleChange = (_event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
@@ -54,8 +79,43 @@ const Books: React.FC = () => {
     <div
       style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
     >
-      <Box sx={{ display: "flex", justifyContent: "space-between", p: 2, marginBottom: "10px" }}>
-        <InputFileUpload />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          p: 2,
+          marginBottom: "10px",
+        }}
+      >
+        <Box sx={{ display: "flex", gap: "10px" }}>
+          <InputFileUpload />
+          <Tooltip title="Help">
+            <IconButton onClick={handleOpen}>
+              <HelpOutlineIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              How to find a file to upload?
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Go to the{" "}
+              <Link to={"https://www.goodreads.com/review/import"}>
+                https://www.goodreads.com/review/import
+              </Link>
+              , click the "Export Library" button, next after a while there will
+              be a text "Your export from ...". Click on it and file will be
+              downloaded. Now you can upload it here.
+            </Typography>
+          </Box>
+        </Modal>
         <Button
           variant="contained"
           color="primary"
@@ -66,7 +126,10 @@ const Books: React.FC = () => {
           Add Book
         </Button>
       </Box>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 3, sm: 6, md: 9 }}
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 3, sm: 6, md: 9 }}
       >
         {paginatedBooks.map((book) => (
           <Grid size={3} key={book.id}>
