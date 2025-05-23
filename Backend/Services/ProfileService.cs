dcb16e999f9b47d644f4d0d7bedd82e8a949cee1
gregusio <grzegorzdynaq@gmail.com>
2025-05-23 20:15:26 +0200

    Add ProfileController, UserProfileDTO, and IProfileService implementations for user profile management

---
+++ Backend/Services/ProfileService.cs
@@ -0,0 +1,35 @@
+using Backend.DTO;
+using Backend.Interfaces;
+using Backend.Repositories;
+
+namespace Backend.Services;
+
+public class ProfileService(IUserActivityHistoryService userActivityHistoryService, UserRepository userRepository) : IProfileService
+{
+    private readonly IUserActivityHistoryService _userActivityHistoryService = userActivityHistoryService;
+    private readonly UserRepository _userRepository = userRepository;
+
+    public async Task<UserProfileDTO> GetUserProfile(int userId)
+    {
+        var user = await _userRepository.GetByIdAsync(userId) ?? throw new Exception("User not found");
+        var userActivityHistory = await _userActivityHistoryService.GetUserActivityHistory(userId);
+        var userProfileDto = new UserProfileDTO
+        {
+            Id = user.Id,
+            Username = user.Username,
+            AvatarUrl = string.Empty, // TODO: avatarUrl should be added to the User entity
+            Bio = string.Empty, // TODO: bio should be added to the User entity
+            UserActivityHistory = userActivityHistory
+        };
+        return userProfileDto;
+    }
+
+    public async Task<UserProfileDTO> UpdateUserProfile(int userId, UserProfileDTO userProfileDto)
+    {
+        await _userRepository.UpdateUserAsync(userId, userProfileDto);
+
+        return await GetUserProfile(userId);
+    }
+}
+
+    
