e533a36db658cff36e6e93d604f7aebefb716996
trissve <e.porada8@gmail.com>
2025-05-29 21:09:24 +0200

    Deleting avatar, adding simple css file

--- frontend/src/components/Card/ProfileCard.tsx
+++ frontend/src/components/Card/ProfileCard.tsx
@@ -3,16 +3,14 @@ import { useAuth } from "../../context/AuthContext";
 
 interface ProfileCardProps {
     username: string;
-    avatarUrl: string;
     bio: string;
 }
-const ProfileCard: React.FC<ProfileCardProps> = ({ username, avatarUrl, bio }) => {
+const ProfileCard: React.FC<ProfileCardProps> = ({ username, bio }) => {
     const { user } = useAuth();
 
     return (
         <div className="profile-card">
-            <img src={avatarUrl} alt={`${username}'s avatar`} />
-            <h2>{username}</h2>
+            <h2>Username: {username}</h2>
             <p>{bio}</p>
             {user && <button>Edit Profile</button>}
         </div>
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -4,12 +4,12 @@ import ProfileCard from "../components/Card/ProfileCard";
 import ActivityTimeline from "../components/Timeline/ActivityTimeline";
 import { useNavigate, useParams } from "react-router-dom";
 import Button from '@mui/material/Button'
+import "./Profile.css";
 
 
 const Profile: React.FC = () => {
 
     const [username, setUsername] = React.useState("");
-    const [avatarUrl, setAvatarUrl] = React.useState("");
     const [bio, setBio] = React.useState("");
     const [activityHistory, setActivityHistory] = React.useState([]);
     const userId = useParams<{ userId: string }>().userId;
@@ -23,7 +23,6 @@ const Profile: React.FC = () => {
             }
             const profileData = await profileService.getProfile(userId);
             setUsername(profileData.username);
-            setAvatarUrl(profileData.avatarUrl);
             setBio(profileData.bio);
         } catch (error) {
             console.error("Error fetching user profile:", error);
@@ -52,17 +51,16 @@ const Profile: React.FC = () => {
         <div className="profile-page">
             <div className="profile-container">
                 <h1>Welcome to your profile!</h1>
-                <ProfileCard username={username} avatarUrl={avatarUrl} bio={bio} />
+                <ProfileCard username={username} bio={bio} />
+            </div>
+                <div className="user-friends-list">
+                <Button variant="text" onClick={() => navigate(`/${userId}/following`)}>
+                    View Following
+                </Button>
             </div>
             <div className="user-updates-history">
                 <h2>Your activity history</h2>
                 <ActivityTimeline activities={activityHistory} />
-            </div>
-            <div className="user-friends-list">
-                <Button variant="text" onClick={() => navigate(`/${userId}/following`)}>
-                    View Following
-        
-                </Button>
             </div>
         </div>
     );
