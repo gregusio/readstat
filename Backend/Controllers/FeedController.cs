ecc7247a3688375ed9470f4b49834e98e5c29526
gregusio <grzegorzdynaq@gmail.com>
2025-05-30 14:13:03 +0200

    Refactor GetUserFeed method to retrieve userId from claims instead of as a parameter

--- Backend/Controllers/FeedController.cs
+++ Backend/Controllers/FeedController.cs
@@ -1,5 +1,6 @@
 namespace Backend.Controllers;
 
+using System.Security.Claims;
 using Backend.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,8 +13,9 @@ public class FeedController(IFeedService feedService) : ControllerBase
     private readonly IFeedService _feedService = feedService;
 
     [HttpGet("get")]
-    public async Task<IActionResult> GetUserFeed(int userId)
+    public async Task<IActionResult> GetUserFeed()
     {
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
         var feed = await _feedService.GetUserFeedAsync(userId);
         return Ok(feed);
     }
