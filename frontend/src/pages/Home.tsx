be3fa25ed1c223cbf2260a42f64390ad321aa016
gregusio <grzegorzdynaq@gmail.com>
2026-02-24 19:32:10 +0100

    test: verify Azure deployment

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Home: React.FC = () => {
-  return (
-    <div>
-      <h1>Welcome Home!</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome Home!</h1>
+            <p>🚀 Deployed to Azure successfully!</p>
+        </div>
+    );
 };
 
 export default Home;
