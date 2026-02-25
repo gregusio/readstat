6fad71a52b53de23c8d4579b78385e17371adfc7
gregusio <grzegorzdynaq@gmail.com>
2026-02-25 20:16:40 +0100

    Set loading false after user fetch

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -2,82 +2,82 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 import apiClient from "../services/apiClient";
 
 interface User {
-  id: string;
-  username: string;
+    id: string;
+    username: string;
 }
 
 interface AuthContextType {
-  isAuthenticated: boolean;
-  user: User | null;
-  setUser: (user: User | null) => void;
-  loading: boolean;
-  login: () => void;
-  logout: () => void;
+    isAuthenticated: boolean;
+    user: User | null;
+    setUser: (user: User | null) => void;
+    loading: boolean;
+    login: () => void;
+    logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
+    children,
 }) => {
-  const [user, setUser] = useState<User | null>(null);
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
+    const [user, setUser] = useState<User | null>(null);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const token = localStorage.getItem("accessToken");
-      if (token) {
-        try {
-          const response = await apiClient.get("/Auth/me");
-          setIsAuthenticated(true);
-          setLoading(false);
-          setUser(response.data);
-        } catch (error) {
-          console.error("Error fetching user:", error);
-          setUser(null);
+    useEffect(() => {
+        const fetchUser = async () => {
+            const token = localStorage.getItem("accessToken");
+            if (token) {
+                try {
+                    const response = await apiClient.get("/Auth/me");
+                    setIsAuthenticated(true);
+                    setUser(response.data);
+                } catch (error) {
+                    console.error("Error fetching user:", error);
+                    setUser(null);
+                }
+            }
+            setLoading(false);
+        };
+
+        fetchUser();
+    }, []);
+
+    const login = async () => {
+        setLoading(false);
+        setIsAuthenticated(true);
+        localStorage.removeItem("userBooks");
+        const response = await apiClient.get("/Auth/me");
+        if (response.data) {
+            setUser(response.data);
+        } else {
+            console.error("Login failed, no user data returned");
+            setUser(null);
         }
-      }
     };
 
-    fetchUser();
-  }, []);
+    const logout = () => {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        localStorage.removeItem("userBooks");
+        setIsAuthenticated(false);
+        setUser(null);
+        setLoading(true);
+    };
 
-  const login = async () => {
-    setLoading(false);
-    setIsAuthenticated(true);
-    localStorage.removeItem("userBooks");
-    const response = await apiClient.get("/Auth/me");
-    if (response.data) {
-      setUser(response.data);
-    } else {
-      console.error("Login failed, no user data returned");
-      setUser(null);
-    }
-  };
-
-  const logout = () => {
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
-    localStorage.removeItem("userBooks");
-    setIsAuthenticated(false);
-    setUser(null);
-    setLoading(true);
-  };
-
-  return (
-    <AuthContext.Provider
-      value={{ isAuthenticated, user, setUser, loading, login, logout }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider
+            value={{ isAuthenticated, user, setUser, loading, login, logout }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider");
-  }
-  return context;
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
 };
